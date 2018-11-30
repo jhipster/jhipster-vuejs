f3cd6b8ce128ee1fbb9e282734c597332204b1f2
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-11-30 19:48:18 +0100

    Add npm test for blueprint to check generated files

--- test/client.spec.js
+++ test/client.spec.js
@@ -2,9 +2,10 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const constants = require('generator-jhipster/generators/generator-constants');
-const VueJSClientGenerator = require('../generators/client/index');
 
 const CLIENT_MAIN_SRC_DIR = constants.CLIENT_MAIN_SRC_DIR;
+const CLIENT_TEST_SRC_DIR = constants.CLIENT_TEST_SRC_DIR;
+const CLIENT_WEBPACK_DIR = constants.CLIENT_WEBPACK_DIR;
 
 const expectedFiles = {
     i18nJson: [
@@ -40,49 +41,296 @@ const expectedFiles = {
         `${CLIENT_MAIN_SRC_DIR}i18n/fr/settings.json`,
         `${CLIENT_MAIN_SRC_DIR}i18n/fr/reset.json`,
         `${CLIENT_MAIN_SRC_DIR}i18n/fr/user-management.json`
+    ],
+
+    app: [
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/change-password/ChangePassword.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/change-password/ChangePassword.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/login-form/LoginForm.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/login-form/LoginForm.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/register/Register.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/register/Register.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/reset-password/ResetPassword.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/reset-password/ResetPassword.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/sessions/Sessions.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/sessions/Sessions.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/settings/Settings.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/settings/Settings.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/LoginModalService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/Principal.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/account/RegisterService.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/audits/Audits.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/audits/AuditsService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/configuration/Configuration.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/configuration/Configuration.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/configuration/ConfigurationService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/docs/Docs.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/docs/Docs.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/health/Health.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/health/Health.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/health/HealthModal.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/health/HealthService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/logs/Logs.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/logs/LogsService.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/metrics/Metrics.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/metrics/Metrics.modal.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/metrics/Metrics.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/metrics/MetricsModal.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/metrics/MetricsService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagement.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagement.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagementEdit.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagementEdit.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagementService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagementView.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/admin/user-management/UserManagementView.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/components/home/Home.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/home/Home.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/jhi-footer/JhiFooter.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/jhi-footer/JhiFooter.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/jhi-navbar/JhiNavbar.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/jhi-navbar/JhiNavbar.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/ribbon/Ribbon.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/components/ribbon/Ribbon.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/config/axios-interceptor.ts`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/entities/user/user.service.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/locale/LanguageService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/locale/TranslationService.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/router/index.ts`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/shared/data/DataUtilsService.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/shared/date/filters.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/shared/config.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/shared/ItemCount.vue`,
+
+        `${CLIENT_MAIN_SRC_DIR}app/App.component.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/App.vue`,
+        `${CLIENT_MAIN_SRC_DIR}app/constants.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/main.ts`,
+        `${CLIENT_MAIN_SRC_DIR}app/shims-vue.d.ts`
+    ],
+
+    test: [
+        // `${CLIENT_TEST_SRC_DIR}jest.conf.js`,
+    ],
+
+    protractor: [
+        `${CLIENT_TEST_SRC_DIR}e2e/modules/account/account.spec.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/modules/administration/administration.spec.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/base-component.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/navbar-page.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/password-page.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/register-page.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/settings-page.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/page-objects/signin-page.ts`,
+        `${CLIENT_TEST_SRC_DIR}e2e/util/utils.ts`,
+        `${CLIENT_TEST_SRC_DIR}protractor.conf.js`
+    ],
+
+    webpack: [
+        `${CLIENT_WEBPACK_DIR}/loader.conf.js`,
+        `${CLIENT_WEBPACK_DIR}/utils.js`,
+        `${CLIENT_WEBPACK_DIR}/vue.utils.js`,
+        `${CLIENT_WEBPACK_DIR}/webpack.common.js`,
+        `${CLIENT_WEBPACK_DIR}/webpack.dev.js`,
+        `${CLIENT_WEBPACK_DIR}/webpack.prod.js`
     ]
 };
 
-describe('JHipster client generator with blueprint', () => {
-    const blueprintNames = ['generator-jhipster-vuejs', 'vuejs'];
-
-
-    blueprintNames.forEach((blueprintName) => {
-        describe(`generate client with blueprint option '${blueprintName}'`, () => {
-            before((done) => {
-                helpers
-                    .run(path.join(__dirname, '../node_modules/generator-jhipster/generators/client'))
-                    .withOptions({
-                        'from-cli': true,
-                        build: 'gradle',
-                        auth: 'jwt',
-                        db: 'mysql',
-                        skipInstall: true,
-                        blueprint: blueprintName,
-                        skipChecks: true
-                    })
-                    .withGenerators([[VueJSClientGenerator, 'jhipster-vuejs:client']])
-                    .withPrompts({
-                        baseName: 'jhipster',
-                        clientFramework: 'VueJS',
-                        useSass: false,
-                        enableTranslation: true,
-                        nativeLanguage: 'en',
-                        languages: ['fr']
-                    })
-                    .on('end', done());
-            });
-
-            it('creates expected files from jhipster client generator', () => {
-                // assert.file(expectedFiles.client);
-                assert.file(expectedFiles.i18nJson);
-            });
-
-            it('contains the specific change added by the blueprint', () => {
-                assert.fileContent('package.json', '"vue"');
-                assert.fileContent('package.json', '"vuex"');
-                assert.fileContent('package.json', '"vuelidate"');
-            });
+describe('VueJS JHipster blueprint', () => {
+    describe('Default with Maven', () => {
+        before((done) => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'vuejs',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/client/index.js'), // eslint-disable-line global-require
+                        'jhipster-vuejs:client',
+                        path.join(__dirname, '../generators/client/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    baseName: 'sampleMysql',
+                    packageName: 'com.mycompany.myapp',
+                    applicationType: 'monolith',
+                    databaseType: 'sql',
+                    devDatabaseType: 'h2Disk',
+                    prodDatabaseType: 'mysql',
+                    cacheProvider: 'ehcache',
+                    authenticationType: 'jwt',
+                    enableTranslation: true,
+                    nativeLanguage: 'en',
+                    languages: ['en', 'fr'],
+                    buildTool: 'maven',
+                    clientFramework: 'VueJS'
+                })
+                .on('end', done);
+        });
+        it('creates expected files from jhipster client generator', () => {
+            assert.file(expectedFiles.i18nJson);
+            assert.file(expectedFiles.app);
+            assert.file(expectedFiles.test);
+            assert.noFile(expectedFiles.protractor);
+            assert.file(expectedFiles.webpack);
+        });
+        it('contains the specific change added by the blueprint', () => {
+            assert.fileContent('package.json', '"vue"');
+            assert.fileContent('package.json', '"vuex"');
+            assert.fileContent('package.json', '"vuelidate"');
+        });
+    });
+    describe('Default with Gradle', () => {
+        before((done) => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'vuejs',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/client/index.js'), // eslint-disable-line global-require
+                        'jhipster-vuejs:client',
+                        path.join(__dirname, '../generators/client/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    baseName: 'sampleMysql',
+                    packageName: 'com.mycompany.myapp',
+                    applicationType: 'monolith',
+                    databaseType: 'sql',
+                    devDatabaseType: 'h2Disk',
+                    prodDatabaseType: 'mysql',
+                    cacheProvider: 'ehcache',
+                    authenticationType: 'jwt',
+                    enableTranslation: true,
+                    nativeLanguage: 'en',
+                    languages: ['en', 'fr'],
+                    buildTool: 'gradle',
+                    clientFramework: 'VueJS'
+                })
+                .on('end', done);
+        });
+        it('creates expected files from jhipster client generator', () => {
+            assert.file(expectedFiles.i18nJson);
+            assert.file(expectedFiles.app);
+            assert.file(expectedFiles.test);
+            assert.noFile(expectedFiles.protractor);
+            assert.file(expectedFiles.webpack);
+        });
+        it('contains the specific change added by the blueprint', () => {
+            assert.fileContent('package.json', '"vue"');
+            assert.fileContent('package.json', '"vuex"');
+            assert.fileContent('package.json', '"vuelidate"');
+        });
+    });
+    describe('noi18n with Maven', () => {
+        before((done) => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'vuejs',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/client/index.js'), // eslint-disable-line global-require
+                        'jhipster-vuejs:client',
+                        path.join(__dirname, '../generators/client/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    baseName: 'sampleMysql',
+                    packageName: 'com.mycompany.myapp',
+                    applicationType: 'monolith',
+                    databaseType: 'sql',
+                    devDatabaseType: 'h2Disk',
+                    prodDatabaseType: 'mysql',
+                    cacheProvider: 'ehcache',
+                    authenticationType: 'jwt',
+                    enableTranslation: false,
+                    nativeLanguage: 'en',
+                    buildTool: 'maven',
+                    clientFramework: 'VueJS'
+                })
+                .on('end', done);
+        });
+        it('creates expected files from jhipster client generator', () => {
+            assert.noFile(expectedFiles.i18nJson);
+            assert.file(expectedFiles.app);
+            assert.file(expectedFiles.test);
+            assert.noFile(expectedFiles.protractor);
+            assert.file(expectedFiles.webpack);
+        });
+        it('contains the specific change added by the blueprint', () => {
+            assert.fileContent('package.json', '"vue"');
+            assert.fileContent('package.json', '"vuex"');
+            assert.fileContent('package.json', '"vuelidate"');
+        });
+    });
+    describe('Elasticsearch and Protractor', () => {
+        before((done) => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'vuejs',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/client/index.js'), // eslint-disable-line global-require
+                        'jhipster-vuejs:client',
+                        path.join(__dirname, '../generators/client/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    baseName: 'sampleMysql',
+                    packageName: 'com.mycompany.myapp',
+                    applicationType: 'monolith',
+                    databaseType: 'sql',
+                    devDatabaseType: 'h2Disk',
+                    prodDatabaseType: 'mysql',
+                    cacheProvider: 'ehcache',
+                    authenticationType: 'jwt',
+                    searchEngine: 'elasticsearch',
+                    enableTranslation: true,
+                    nativeLanguage: 'en',
+                    languages: ['en', 'fr'],
+                    testFrameworks: ['protractor'],
+                    buildTool: 'maven',
+                    clientFramework: 'VueJS'
+                })
+                .on('end', done);
+        });
+        it('creates expected files from jhipster client generator', () => {
+            assert.file(expectedFiles.i18nJson);
+            assert.file(expectedFiles.app);
+            assert.file(expectedFiles.test);
+            assert.file(expectedFiles.protractor);
+            assert.file(expectedFiles.webpack);
+        });
+        it('contains the specific change added by the blueprint', () => {
+            assert.fileContent('package.json', '"vue"');
+            assert.fileContent('package.json', '"vuex"');
+            assert.fileContent('package.json', '"vuelidate"');
         });
     });
 });
